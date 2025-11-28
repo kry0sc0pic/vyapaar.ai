3119aa1152a2e9e9187a5102a7747505a29b096d
Naman Narula <naman@narula.dev>
2025-11-28 23:23:25 +0530

    moving DEFAULT_INSTRUCTIONS to separate file
    Removed default instructions from configuration.

--- config/__init__.py
+++ config/__init__.py
@@ -2,9 +2,6 @@
 GEMINI_VOICE = "Charon"
 GEMINI_MODEL = "gemini-2.5-flash-native-audio-preview-09-2025"
 
-# Instructions
-DEFAULT_INSTRUCTIONS = ""
-
 # VAD Configurations
 VAD_CONFIDENCE = 0.7
 VAD_START_SECS = 0.2
