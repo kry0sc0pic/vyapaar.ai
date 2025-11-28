be5b77c641aae4bd470e98e68296482f5e1f2195
Suyash Mittal <44423507+suyashmittal@users.noreply.github.com>
2025-11-29 03:54:02 +0530

    Change VAD_STOP_SECS from 0.8 to 0.3

--- config/__init__.py
+++ config/__init__.py
@@ -5,5 +5,5 @@ GEMINI_MODEL = "gemini-2.5-flash-native-audio-preview-09-2025"
 # VAD Configurations
 VAD_CONFIDENCE = 0.7
 VAD_START_SECS = 0.2
-VAD_STOP_SECS = 0.8
+VAD_STOP_SECS = 0.3
 VAD_MIN_VOLUME = 0.6
